f15878f6c551c2abaa2bf85ac5bc00e7c3fb816a
blueCat11 <blue.cat@t-online.de>
2016-07-31 15:04:15 +0200

    //try 2 res files added

---
+++ android/src/de/mi/ur/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.mi.ur;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.mi.ur.NerdyNumeralChallenge;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new NerdyNumeralChallenge(), config);
+	}
+}
